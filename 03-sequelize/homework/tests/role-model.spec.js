bcf5a38dbec90c2449edc294c4b63d0407c389fa
santiago montanaro <santmon99@gmail.com>
2022-09-22 20:03:21 -0300

    clean repo

---
+++ 03-sequelize/homework/tests/role-model.spec.js
@@ -0,0 +1,33 @@
+const { db, Role } = require('../db');
+
+describe('Role Model', () => {
+  beforeAll(async () => {
+    await db.sync({ force: true });
+  });
+
+  describe('Parte UNO', () => {
+    xit('should not create the Role if name is not send', async () => {
+      expect.assertions(1);
+      try {
+        await Role.create({description: 'Big amount of HP needed to withstands the attacks'});
+      } catch (error) {
+        expect(error.message).toBeDefined();
+      }
+    });
+  
+    xit('should create the Role if all required properties are ok', async () => {
+      const role = await Role.create({
+        name: 'Tank',
+        description: 'Big amount of HP needed to withstands the attacks'
+      });
+      expect(role.toJSON()).toHaveProperty('name','Tank');
+      expect(role.toJSON()).toHaveProperty('description','Big amount of HP needed to withstands the attacks');
+    });
+  })
+
+
+  afterAll(async () => {
+    await db.sync({ force: true });
+    db.close();
+  })
+});
